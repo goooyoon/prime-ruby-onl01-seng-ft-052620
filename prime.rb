5d17d761acbe5aec06a0e895e33f3478d602f4f9
Grace Kim <goooyoon@gmail.com>
2020-05-30 02:06:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,14 @@
 def prime?(number)
-
+  if number <= 1
+    return false
+  end
+  i=2
+  while i <= number/2
+    if number % i == 0
+      return false
+    else
+      i += 1
+    end
+  end
+  true
 end
